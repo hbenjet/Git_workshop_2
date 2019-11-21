824011f7839f152c214aa8f565f2cf9cb762efa2
hbenjet <57769844+hbenjet@users.noreply.github.com>
2019-11-21 14:40:59 -0700

    Add files via upload

---
+++ Lab1B-Fibonacci.py
@@ -0,0 +1,19 @@
+#Hannah Benjet 
+#CSCI 101 – Section G
+#Python Lab 1B
+
+f0 = 1
+f1 = 1
+f2 = f0 + f1
+f3 = f1 + f2
+f4 = f2 + f3
+f5 = f3 + f4
+f6 = f4 + f5
+f7 = f5 + f6
+f8 = f6 + f7
+f9 = f7 + f8
+f10 = f8 + f9
+f11 = f9 + f10
+f12 = f10 + f11
+
+print (f0, f1, f2, f3, f5, f6, f7, f8, f9, f10, f11, f12)
